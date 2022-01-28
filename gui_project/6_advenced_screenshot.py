4dc46c9d67e8605df4e90ec69ed8d9faed147443
ilsung12 <a01041118225@gmail.com>
2022-01-28 15:05:08 +0900

    📝 Learn : tkinter - Srcshot Project ft.keyboard

---
+++ gui_project/6_advenced_screenshot.py
@@ -0,0 +1,16 @@
+# pip install keyboard
+import time
+import keyboard
+from PIL import ImageGrab
+
+
+def screenshot():
+    # 2022년 1월 1일 10시 20분 30초 -> _20220101_102030
+    curr_time = time.strftime("_%Y%m%d_%H%M%S")
+    img = ImageGrab.grab()
+    img.save('image{}.png'.format(curr_time)) # ex) image_20220101_102030.png
+
+keyboard.add_hotkey('F9', screenshot) # 사용자가 F9 키를 누르면 스크린샷 저장
+#keyboard.add_hotkey('crtl+shift+s', screenshot) 
+
+keyboard.wait('esc') # 사용자가 ESC 를 누를때까지 프로그램 수행
