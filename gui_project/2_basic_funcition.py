477eff1ab9e899a62572f7010091d3f4e3fa00ed
ilsung12 <a01041118225@gmail.com>
2022-01-28 11:08:45 +0900

    📝 Learn : tkinter - basic_function

--- gui_project/2_basic_funcition.py
+++ gui_project/2_basic_funcition.py
@@ -1,19 +1,69 @@
 import tkinter.ttk as ttk
-from tkinter import *
-from turtle import width
+import tkinter.messagebox as msgbox
+from tkinter import * # __all__ 에 정의되지 않았다면 import 를 하지않음.
+from tkinter import filedialog # 파일 다이얼로그 = 서브 모듈이기 때문에 별도로 import 해주어야한다.
+
 
 root = Tk()
 root.title('image SUM project')
+
+# 파일 추가
+def add_file():
+    # 복수개의 파일 선택
+    files = filedialog.askopenfilenames(title='이미지 파일을 선택하세요', \
+        filetypes=(('PNG 파일', '*.png'), ('모든 파일', '*.*')),
+        initialdir=r'C:\Users\user\Desktop\velohg') # 최초에 사용자가 지정한 경로를 보여줌
+
+    # 사용자가 선택한 파일 목록
+    for file in files:
+        list_file.insert(END, file)
+
+# 파일 삭제
+def del_file():
+    #print(list_file.curselection())
+    for index in reversed(list_file.curselection()):
+        list_file.delete(index)
+
+# 저장 경로(폴더)
+def browse_dest_path():
+    folder_selected = filedialog.askdirectory()
+    if folder_selected is None: # 사용자가 취소를 누를 때
+        return
+    #print(folder_selected)
+    ent_dest_path.delete(0, END) # Text 일경우, ('1.0', END)
+    ent_dest_path.insert(0, folder_selected)
+
+
+# 시작
+def start():
+    # 각 옵션들 값을 확인
+    print('가로넓이: ', cmb_width.get())
+    print('간격: ', cmb_space.get())
+    print('포맷: ', cmb_format.get())
+
+    # 파일 목록 확인
+    if list_file.size() == 0:
+        msgbox.showwarning('경고', '이미지 파일을 추가하세요.')
+        return
+
+    # 저장 경로 확인
+    if len(ent_dest_path.get()) == 0:
+        msgbox.showwarning('경고', '저장 경로를 선택하세요.')
+        return
+        
+
 
 # 파일 프레임 (파일 추가, 선택 삭제)
 file_frame = Frame(root)
 file_frame.pack(fill='x', padx=5, pady=5)
 
-btn_add_file = Button(file_frame, padx=5, pady=5, width=12, text='파일추가')
+btn_add_file = Button(file_frame, padx=5, pady=5, width=12, text='파일추가', command=add_file)
 btn_add_file.pack(side='left')
 
-btn_del_file = Button(file_frame, padx=5, pady=5, width=12, text='파일삭제')
+btn_del_file = Button(file_frame, padx=5, pady=5, width=12, text='선택삭제', command=del_file)
 btn_del_file.pack(side='right')
+
+
 
 # 리스트 프레임
 list_frame = Frame(root)
@@ -26,20 +76,25 @@ list_file = Listbox(list_frame, selectmode='extended', height=15, yscrollcommand
 list_file.pack(side='left', fill='both', expand=True)
 scrollbar.config(command=list_file.yview)
 
-# 저장 경로 프레임
 
+
+# 저장 경로 프레임
 path_frame = LabelFrame(root, text='저장경로')
 path_frame.pack(fill='x', padx=5, pady=5, ipady=5)
 
-txt_dest_path = Entry(path_frame)
-txt_dest_path.pack(side='left', fill='x', expand=True, ipady=4, padx=5, pady=5) # 높이 변경
+ent_dest_path = Entry(path_frame)
+ent_dest_path.pack(side='left', fill='x', expand=True, ipady=4, padx=5, pady=5) # 높이 변경
 
-btn_dest_path = Button(path_frame, text='찾아보기', width=10)
+btn_dest_path = Button(path_frame, text='찾아보기', width=10, command=browse_dest_path)
 btn_dest_path.pack(side='right', padx=5, pady=5)
+
+
 
 # 옵션 프레임
 frame_option = LabelFrame(root, text='옵션')
 frame_option.pack(padx=5, pady=5, ipady=5)
+
+
 
 # 1. 가로넓이 옵션
 # 가로 넓이 레이블
@@ -52,6 +107,8 @@ cmb_width = ttk.Combobox(frame_option, state='readonly', values=opt_width, width
 cmb_width.current(0)
 cmb_width.pack(side='left', padx=5, pady=5)
 
+
+
 # 2. 간격 옵션
 # 간격 옵션 레이블
 lbl_space = Label(frame_option, text='간격', width=8)
@@ -62,6 +119,8 @@ opt_space = ['없음', '좁게', '보통', '넓게']
 cmb_space = ttk.Combobox(frame_option, state='readonly', values=opt_space, width=10)
 cmb_space.current(0)
 cmb_space.pack(side='left', padx=5, pady=5)
+
+
 
 # 3. 파일 포멧 옵션
 # 파일 포멧 옵션 레이블
@@ -89,7 +148,7 @@ frame_run.pack(fill='x', padx=5, pady=5)
 btn_close = Button(frame_run, padx=5, pady=5, text='닫기', width=12, command=root.quit)
 btn_close.pack(side='right', padx=5, pady=5)
 
-btn_start = Button(frame_run, padx=5, pady=5, text='시작', width=12)
+btn_start = Button(frame_run, padx=5, pady=5, text='시작', width=12, command=start)
 btn_start.pack(side='right', padx=5, pady=5)
 
 
