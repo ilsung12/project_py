4f5bd9398c3368e98cdd1a72b201c5cee4f5fa36
ilsung12 <a01041118225@gmail.com>
2022-02-25 09:06:53 +0900

    📝 Learn : Add Text

--- socket/echo_thread/simplel_test/ehcoCli.py
+++ socket/echo_thread/simplel_test/ehcoCli.py
@@ -12,3 +12,11 @@ def run_client(host='127.0.0.1', port=7788):
             print(res.decode())
 if __name__ == '__main__':
     run_client()
+
+# 클라이언트 : 바인드나 리스닝의 과정이 필요없다는 것
+# 필요 목록 : ip, host, buf_size
+# 1. 소켓 열기
+# 2. 소켓 연결(connect) : 
+# 3. 데이터 보내기 : sock.sendall()
+# 4. 데이터 받기 : sock.recv()
+# 5. close
